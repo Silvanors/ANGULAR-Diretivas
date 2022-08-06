0bd9f1809613b13906b12a371d8c5b29644a5cc4
Silvanors <silvanors@gmail.com>
2022-08-06 16:55:22 -0300

    aplicação de diretivas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,12 +10,37 @@ import { Component, OnInit } from '@angular/core';
 export class AppComponent implements OnInit {
   title = 'AMBIENTE-ANGULAR';
   count = 0;
+  nome = 'Ivonaldo Junior Escobar Soares';
+  text: string = '';
+
+  pessoas = [
+    {
+      nome: "Ivonaldo",
+      sobrenome: "Soares"  
+    },
+    {
+      nome: "Joao",
+      sobrenome: "Pereira"   
+
+    },
+    {
+      nome: "Marcio",
+      sobrenome: "Santos"  
+
+    },
+    {
+      nome: "Maria",
+      sobrenome: "Silva"  
+
+    }
+  ]
 
   constructor () {
 
   }
 
   ngOnInit() {
+    console.log(this.pessoas)
     let interval = setInterval(() => {
       this.count++;
       if(this.count === 10) {
@@ -23,5 +48,11 @@ export class AppComponent implements OnInit {
       }
     },1000)
   }
+
+  clicou(nome:string): void {
+    console.log('Clicou em mim', nome)
+  }
+
+
 
 }
